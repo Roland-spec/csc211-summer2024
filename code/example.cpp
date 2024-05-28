11ed31f644b0b9d04eb2d577a4055b6bb09b61bb
Michael Conti <michaelconti@uri.edu>
2024-05-28 14:00:15 -0400

    lec03

---
+++ code/example.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int main(){
+
+    int a = 0;
+    int b;
+    int c;
+
+    std::cout << a << std::endl;
+    std::cout << b << std::endl;
+    std::cout << c << std::endl;
+
+
+    std::cout << &a << std::endl;
+    std::cout << &b << std::endl;
+    std::cout << &c << std::endl;
+
+
+    return 0;
+}
